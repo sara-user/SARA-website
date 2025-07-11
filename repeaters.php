d613d4094ae8edc7781cdb78ac0ced31911b54c3
dick lieber <dick@u505.com>
2025-07-11 12:09:40 -0500

    Repeater phjoto

--- repeaters.php
+++ repeaters.php
@@ -1,49 +1,54 @@
-
 <?php include 'includes/header.php'; ?>
+<p>Located in the Lincoln Park Community of Chicago.</p>
 
-<div class="container">
-    <p>Located in the Lincoln Park Community of Chicago.</p>
-    <table class="table">
-        <thead class="table-primary">
-        <tr>
-            <th>Repeater</th>
-            <th>Mode</th>
-            <th>Frequency</th>
-            <th>Offset</th>
-            <th>CTCSS</th>
-            <th>Antenna</th>
-            <th>Controller</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td>KA9HHH</td>
-            <td>Analog FM</td>
-            <td>146.880 Mhz</td>
-            <td> -.600 Mhz</td>
-            <td>107.2.8</td>
-            <td>4-bay Folded Dipole</td>
-            <td>RC-210</td>
-        </tr>
-        <tr>
-            <td>KA9HHH</td>
-            <td>Analog FM</td>
-            <td>444.375 Mhz</td>
-            <td>+5.00 Mhz</td>
-            <td>114.8</td>
-            <td>8-bay Folded Dipole</td>
-            <td>RC-210</td>
-        </tr>
-        <tr>
-            <td title="Polish-American Ham Radio Operators Society"><a href="https://paaros.com" target="_blank">NA9PL</a></td>
-            <td>Fusion/Wires-X</td>
-            <td>440.250 Mhz</td>
-            <td>+5.00 Mhz</td>
-            <td>114.8</td>
-            <td>8-bay Folded Dipole</td>
-            <td>Yaesu DR</td>
-        </tr>
-        </tbody>
-    </table>
+<div class="row">
+    <div class="col">
+        <table class="table">
+            <thead class="table-primary">
+            <tr>
+                <th>Repeater</th>
+                <th>Mode</th>
+                <th>Frequency</th>
+                <th>Offset</th>
+                <th>CTCSS</th>
+                <th>Antenna</th>
+                <th>Controller</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>KA9HHH</td>
+                <td>Analog FM</td>
+                <td>146.880 Mhz</td>
+                <td> -.600 Mhz</td>
+                <td>107.2.8</td>
+                <td>4-bay Folded Dipole</td>
+                <td>RC-210</td>
+            </tr>
+            <tr>
+                <td>KA9HHH</td>
+                <td>Analog FM</td>
+                <td>444.375 Mhz</td>
+                <td>+5.00 Mhz</td>
+                <td>114.8</td>
+                <td>8-bay Folded Dipole</td>
+                <td>RC-210</td>
+            </tr>
+            <tr>
+                <td title="Polish-American Ham Radio Operators Society"><a href="https://paaros.com"
+                                                                           target="_blank">NA9PL</a></td>
+                <td>Fusion/Wires-X</td>
+                <td>440.250 Mhz</td>
+                <td>+5.00 Mhz</td>
+                <td>114.8</td>
+                <td>8-bay Folded Dipole</td>
+                <td>Yaesu DR</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="col">
+        <img src="images/vhfUhfrepeaters.jpeg" width="300">
+    </div>
 </div>
 <?php include 'includes/footer.php'; ?>
