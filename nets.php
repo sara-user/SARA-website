9663721327fff5ba5978f5567e65b3b80ec5eaf7
dick lieber <dick@u505.com>
2025-07-11 12:10:17 -0500

    Nets: add WCARS, CERT.

--- nets.php
+++ nets.php
@@ -16,6 +16,17 @@
             <td>146.880 Mhz</td>
             <td>Mondays 7PM</td>
         </tr>
+        <tr>
+            <td><a href="https://wcars.radio" target="_blank" title="Windy City Amateur Radio Society">WCARS</td>
+            <td>146.880 Mhz</td>
+            <td>Thursdays 7PM</td>
+        </tr>
+        <tr>
+            <td><a href="https://www.chicago.gov/city/en/depts/oem/provdrs/edu/svcs/become_a_cert_volunteer.html" target="_blank" title="Chicago Suburban Radio Association">CERT</td>
+            <td>146.880 Mhz</td>
+            <td>do we still need this?</td>
+        </tr>
+
         </tbody>
     </table>
 </div>
