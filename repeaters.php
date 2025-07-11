3dae1acf34dc473fffb78c815be82ea9040e84b3
dick lieber <dick@u505.com>
2025-07-11 10:20:16 -0500

    Add picnic-2025.php

--- repeaters.php
+++ repeaters.php
@@ -7,6 +7,7 @@
         <thead class="table-primary">
         <tr>
             <th>Repeater</th>
+            <th>Mode</th>>
             <th>Frequency</th>
             <th>Offset</th>
             <th>CTCSS</th>
@@ -14,14 +15,23 @@
         </thead>
         <tbody>
         <tr>
-            <td>Analog VHF</td>
+            <td>KA9HHH</td>
+            <td>Analog FM</td>
             <td>146.880 Mhz</td>
             <td> -.600 Mhz</td>
             <td>107.2.8</td>
         </tr>
         <tr>
-            <td>Analog UHF</td>
+            <td>KA9HHH</td>
+            <td>Analog FM</td>
             <td>444.375 Mhz</td>
+            <td>+5.00 Mhz</td>
+            <td>114.8</td>
+        </tr>
+        <tr>
+            <td title="Polish-American Ham Radio Operators Society"><a href="https://paaros.com" target="_blank">NA9PL</a></td>
+            <td>Fusion/Wires-X</td>
+            <td>440.250 Mhz</td>
             <td>+5.00 Mhz</td>
             <td>114.8</td>
         </tr>
