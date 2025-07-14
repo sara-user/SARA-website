b7f58d1bfc2284b744798c670788d627ec23cc99
dick lieber <dick@u505.com>
2025-07-14 12:31:43 -0500

    Fix hamburger nav

--- includes/footer.php
+++ includes/footer.php
@@ -2,5 +2,7 @@
 <footer>
     <p>&copy; 2025 SARA</p>
 </footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -7,9 +7,7 @@
     <title>SARA</title>
     <link rel="stylesheet" href="/css/styles.css"/>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-          rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-          crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
 
 </head>
     <!-- Google tag (gtag.js) -->
@@ -25,7 +23,9 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="/index.php" title="Suburban Amateur Repeater Association">SARA</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button"
+        data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
